f45faea1bf4c55bacc77db71af093bc3e60cb95b
Lucie Daeye <lucied@mozillafoundation.org>
2020-02-24 11:04:24 +0100

    There's no pt pt_BR issues anymore

--- archive.sh
+++ archive.sh
@@ -8,10 +8,6 @@ touch translations_github_commit_${TRAVIS_COMMIT}
 # Selecting only a file in travis doesn't seem to work, using a dir instead
 mkdir -p to_upload
 
-# Move the pt_BR files to pt so Django can find them
-rm -rf ./donate/locale/pt/
-mv ./donate/locale/pt_BR/ ./donate/locale/pt/
-
 echo "Archiving files"
 tar -C donate -cvf ./to_upload/translations.tar ./locale
 tar rvf ./to_upload/translations.tar ./translations_github_commit_${TRAVIS_COMMIT}
